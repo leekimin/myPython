3a8a6567523a5bc902257b53ca64278fac5d4b56
leekimin <meant00@naver.com>
2024-04-17 18:15:02 +0900

    test

--- 0008_stt/main.py
+++ 0008_stt/main.py
@@ -1,3 +1,4 @@
+"""
 import requests
 filename = "D:\\study\\myPython\\0008_stt\\sample.m4a" 
 def read_file(filename, chunk_size=5242880): 
@@ -12,3 +13,18 @@ headers = {'authorization': "06708306b992491cbb1ad9dfae200e89"}
 response = requests.post('https://api.assemblyai.com/v2/upload', headers=headers, data=read_file(filename)) 
 
 print(response.json())
+"""
+
+import io
+
+file_path = "output.txt"
+
+with open(file_path, "w") as file:
+    for i in range(1000000):
+        print(i)
+        file.write("a")
+
+"""
+for i in range(1000000):
+    print(i)
+"""
